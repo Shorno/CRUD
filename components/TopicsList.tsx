36b2d3c6408afab03a51cf4638012661a8026950
shorno <fb.shorno@gmail.com>
2024-02-13 10:48:14 +0600

    data fetched

--- components/TopicsList.tsx
+++ components/TopicsList.tsx
@@ -6,15 +6,15 @@ import {cache} from "browserslist";
 
 const getTopics = async () => {
     try {
-       const res=  await fetch("http://localhost:3000/api/topics",{
-            cache : "no-store",
+        const res = await fetch("http://localhost:3000/api/topics", {
+            cache: "no-store",
         });
 
-       if (!res.ok){
-           throw new Error("Failed to fetch topics");
-       }
+        if (!res.ok) {
+            throw new Error("Failed to fetch topics");
+        }
 
-       return await res.json();
+        return await res.json();
 
     } catch (error) {
         console.error("Error loading Topics", error);
@@ -23,20 +23,25 @@ const getTopics = async () => {
 };
 export default async function TopicsList() {
     const {topics} = await getTopics();
+
+
     return (
         <>
-            <div className={"p-4 bg-slate-300 my-3 flex justify-between gap-5 items-start"}>
-                <div>
-                    <h2 className={"font-bold text-2xl"}>Topic Title</h2>
-                    <div>Topic Info</div>
+            {topics.map((t : { _id: string, title: string, info: string }) => (
+                <div key={t._id} className={"p-4 bg-slate-300 my-3 flex justify-between gap-5 items-start"}>
+                    <div>
+                        <h2 className={"font-bold text-2xl"}>{t.title}</h2>
+                        <div>{t.info}</div>
+                    </div>
+
+                    <div className={"flex gap-2"}>
+                        <RemoveBtn></RemoveBtn>
+                        <Link href={`/editTopic/${t._id}`}>
+                            <HiPencilAlt size={25}>/</HiPencilAlt>
+                        </Link>
+                    </div>
                 </div>
-                <div className={"flex gap-2"}>
-                    <RemoveBtn></RemoveBtn>
-                    <Link href={"/editTopic/123"}>
-                        <HiPencilAlt size={25}>/</HiPencilAlt>
-                    </Link>
-                </div>
-            </div>
+            ))}
         </>
     )
 }
